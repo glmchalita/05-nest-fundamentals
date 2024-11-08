78fcabaeb4f76f78b28510f8f6dadd4edf28cae0
Guilherme Chalita <guilhermechalita@hotmail.com>
2024-11-08 16:22:31 -0300

    test: Add tests setup files and factories

---
+++ test/factories/make-question-comment.ts
@@ -0,0 +1,23 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import {
+  QuestionComment,
+  QuestionCommentProps,
+} from '@/domain/forum/enterprise/entities/question-comment'
+import { faker } from '@faker-js/faker'
+
+export function makeQuestionComment(
+  overwrite: Partial<QuestionCommentProps> = {},
+  id?: UniqueEntityID,
+) {
+  const questionComment = QuestionComment.create(
+    {
+      authorId: new UniqueEntityID(),
+      questionId: new UniqueEntityID(),
+      content: faker.lorem.text(),
+      ...overwrite,
+    },
+    id,
+  )
+
+  return questionComment
+}
