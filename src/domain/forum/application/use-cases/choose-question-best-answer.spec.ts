e4f72fae08dcd2956dd2659a6a467a54b4b32bb0
Guilherme Chalita <guilhermechalita@hotmail.com>
2024-11-08 16:25:19 -0300

    feat: Add domain layer of application

---
+++ src/domain/forum/application/use-cases/choose-question-best-answer.spec.ts
@@ -0,0 +1,69 @@
+import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { makeAnswer } from 'test/factories/make-answer'
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
+import { ChooseQuestionBestAnswerUseCase } from './choose-question-best-answer'
+import { makeQuestion } from 'test/factories/make-question'
+import { NotAllowedError } from '@/core/errors/not-allowed-error'
+import { InMemoryQuestionAttachmentsRepository } from 'test/repositories/in-memory-question-attachments-repository'
+import { InMemoryAnswerAttachmentsRepository } from 'test/repositories/in-memory-answer-attachments-repository'
+
+describe('Choose Question Best Answer', () => {
+  let inMemoryQuestionAttachmentsRepository: InMemoryQuestionAttachmentsRepository
+  let inMemoryAnswerAttachmentsRepository: InMemoryAnswerAttachmentsRepository
+  let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+  let inMemoryAnswersRepository: InMemoryAnswersRepository
+  let sut: ChooseQuestionBestAnswerUseCase
+
+  beforeEach(() => {
+    inMemoryQuestionAttachmentsRepository =
+      new InMemoryQuestionAttachmentsRepository()
+    inMemoryAnswerAttachmentsRepository =
+      new InMemoryAnswerAttachmentsRepository()
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository(
+      inMemoryQuestionAttachmentsRepository,
+    )
+    inMemoryAnswersRepository = new InMemoryAnswersRepository(
+      inMemoryAnswerAttachmentsRepository,
+    )
+    sut = new ChooseQuestionBestAnswerUseCase(
+      inMemoryQuestionsRepository,
+      inMemoryAnswersRepository,
+    )
+  })
+
+  it('should be able to choose the question best answer', async () => {
+    const question = makeQuestion()
+
+    const answer = makeAnswer({ questionId: question.id })
+
+    await inMemoryQuestionsRepository.create(question)
+
+    await inMemoryAnswersRepository.create(answer)
+
+    await sut.execute({
+      answerId: answer.id.toString(),
+      authorId: question.authorId.toString(),
+    })
+
+    expect(inMemoryQuestionsRepository.items[0].bestAnswerId).toEqual(answer.id)
+  })
+
+  it('should not be able to choose another user question best answer', async () => {
+    const question = makeQuestion({ authorId: new UniqueEntityID('author-1') })
+
+    const answer = makeAnswer({ questionId: question.id })
+
+    await inMemoryQuestionsRepository.create(question)
+
+    await inMemoryAnswersRepository.create(answer)
+
+    const result = await sut.execute({
+      authorId: 'wrong-author',
+      answerId: answer.id.toString(),
+    })
+
+    expect(result.isLeft()).toBe(true)
+    expect(result.value).toBeInstanceOf(NotAllowedError)
+  })
+})
